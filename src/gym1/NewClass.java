5006af5966344295eeffe66377b28d446db43669
Birbal <119029678+birbal3@users.noreply.github.com>
2024-07-05 09:53:28 +0530

    Add files via upload

---
+++ src/gym1/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gym1;
+
+/**
+ *
+ * @author birbal
+ */
+public class NewClass {
+    
+}
